71ecd724f4c5cfad17e36b752a9ee547e50dffb7
vce <937371586@qq.com>
2020-07-11 13:51:14 +0800

    711

---
+++ web/src/main/java/com/weilai9/web/controller/rabbitmq/customer/SmsDirectReceiver.java
@@ -0,0 +1,31 @@
+package com.weilai9.common.config.rabbitmq.customer;
+
+import com.weilai9.common.utils.wechat.HuaweiSmsUtil;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 监听类
+ */
+@Component
+@RabbitListener(queues = "SmsDirectQueue")//监听的队列名称 TestDirectQueue
+public class SmsDirectReceiver {
+
+    @RabbitHandler
+    public void process(Map map) {
+
+        String phoneNum = map.get("phoneNum").toString();
+        String content = map.get("content").toString();
+        try {
+            HuaweiSmsUtil.sendEms(phoneNum,content);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
